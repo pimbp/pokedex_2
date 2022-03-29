28fdbc8901678619ecc0ad4e58c283a51cae9f2e
pimbp <70762327+pimbp@users.noreply.github.com>
2022-03-29 20:00:55 +0200

    filtering done partially
    need to fix it so it searches with ungiven parameters

--- source/index.js
+++ source/index.js
@@ -21,8 +21,14 @@ pokeRouter.use('/', (req, res, next)=>{
 pokeRouter.get('/pokedex', (req, res)=>{
     const id = req.query.id;
     const name = req.query.name;
-    const types = [req.query.type1, req.query.type2]
-    res.status(res.locals.status).json(res.locals.pokemon);
+    const types = [req.query.type1, req.query.type2];
+    let tempArray = [];
+    res.locals.pokemon.forEach(element => {
+        if(element.name.english.includes(name) && element.id == id && ((element.type[0] == types[0] && element.type[1] == types[1]) || (element.type[0] == types[1] && element.type[1] == types[0]))){
+            tempArray.push(element);
+        }
+    });
+    res.status(res.locals.status).json(tempArray);
 });
 
 app.listen(port, function() {
