8d72bbc483c54a106e2e9e4602f636459815e041
pimbp <70762327+pimbp@users.noreply.github.com>
2022-03-31 21:38:02 +0200

    delete endpoint

--- source/pokeRouter.js
+++ source/pokeRouter.js
@@ -1,41 +1,11 @@
 const express = require('express');
+const { returnPokemon, loadPokemon, deletePokemon } = require('./pokedex');
 const pokeRouter = express.Router();
 
-pokeRouter.use('/', (req, res, next)=>{
-    try{
-        res.locals.pokemon = require('../assets/pokedex.json');
-        res.locals.status = 200;
-    }catch{
-        res.locals.status = 500;
-        res.locals.pokemon = ({
-            error: `Error ${res.locals.status} couldn\'t load data`,
-        });
-    }
-    next();
-});
+pokeRouter.use('/', loadPokemon);
 
-pokeRouter.get('/pokedex', (req, res)=>{
-    let tempArray = res.locals.pokemon;
-    if(req.query["name"]){
-        let name = req.query.name;
-        tempArray = tempArray.filter((element)=>{
-            if(element.name.english.toLowerCase().includes(name))
-                return element;
-        });
-    }
-    if(req.query["type"]){
-        let types = req.query.type;
-        tempArray = tempArray.filter((element)=>{
-            if(types.length == 1){
-                if(element.type.find(type => type == types[0]))
-                    return element;
-            }else{
-                if(element.type.find(type => type == types[0]) && element.type.find(type => type == types[1]))
-                    return element;
-            }
-        });
-    }
-    res.status(res.locals.status).json(tempArray);
-});
+pokeRouter.get('/pokedex', returnPokemon);
+
+pokeRouter.delete('/pokedex/:id', deletePokemon);
 
 module.exports = { pokeRouter };
---
+++ source/pokedex.js
@@ -0,0 +1,43 @@
+exports.loadPokemon = (req, res, next)=>{
+    try{
+        res.locals.pokemon = require('../assets/pokedex.json');
+        res.locals.status = 200;
+    }catch{
+        res.locals.status = 500;
+        res.locals.pokemon = ({
+            error: `Error ${res.locals.status} couldn\'t load data`,
+        });
+    }
+    next();
+};
+
+exports.returnPokemon = (req, res)=>{
+    let tempArray = res.locals.pokemon;
+    if(req.query["name"]){
+        let name = req.query.name;
+        tempArray = tempArray.filter((element)=>{
+            if(element.name.english.toLowerCase().includes(name))
+                return element;
+        });
+    }
+    if(req.query["type"]){
+        let types = req.query.type;
+        tempArray = tempArray.filter((element)=>{
+            if(types.length == 1){
+                if(element.type.find(type => type == types[0]))
+                    return element;
+            }else{
+                if(element.type.find(type => type == types[0]) && element.type.find(type => type == types[1]))
+                    return element;
+            }
+        });
+    }
+    res.status(res.locals.status).json(tempArray);
+}
+
+exports.deletePokemon = (req, res) => {
+    res.locals.pokemon.splice(res.locals.pokemon.findIndex((element) => {
+        return element.id == req.params.id;
+    }), 1);
+    res.send('essa')
+};
