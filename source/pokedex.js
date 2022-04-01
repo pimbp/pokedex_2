1ff54c99bf11924fed3ba4009ea7aac86bfc6fb5
test <test@DOM.SCI.EDU.PL>
2022-04-01 09:12:43 +0200

    yummy cummy in my tummy :P

--- source/pokedex.js
+++ source/pokedex.js
@@ -55,6 +55,14 @@ exports.returnPokemon = (req, res)=>{
                 return 1;
             return 0;
         });
+    }else{
+        tempArray.sort((a, b) => {
+            if(a.id > b.id)
+                return 1;
+            if(b.id > a.id)
+                return -1;
+            return 0;
+        });
     }
     res.status(res.locals.status).json(tempArray);
 }
